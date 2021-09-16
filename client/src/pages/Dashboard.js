25d03f3a22e4ed270bd119699943c8c71dcac139
SemaJ Solomon <sxviersolomon@gmail.com>
2021-09-16 16:31:51 -0400

    added dashboard, register, and login routes

---
+++ client/src/pages/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react'
+
+function Dashboard() {
+    useEffect(() => {
+        // fetch('/api/v1/workouts')
+    })
+    return (
+        <div>
+            Dashboard!!!
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ client/src/pages/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { useHistory } from 'react-router'
+
+export default function Login() {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const history = useHistory();
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch('api/v1/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username,
+            email,
+            password
+        })
+    })
+        .then(res => res.json())
+        .then(data => {
+            if (data.error) {
+                setError(data.error)
+            } else {
+                // redirect
+                history.push('/');
+            }
+        })
+}
+
+    return (
+        <div>
+            { error && (<div className="error">{error}</div>) }
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Username</span>
+                    <input value={username} onChange={e => setUsername(e.target.value)}/>
+                </label>
+                <br />
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+                <br />
+                <label>
+                    <span>Password</span>
+                    <input value={password} type="password" onChange={e => setPassword(e.target.value)}/>
+                </label>
+                <br />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
