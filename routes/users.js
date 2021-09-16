800779e9174854b50831f34be5c3ed19b579e76f
SemaJ Solomon <sxviersolomon@gmail.com>
2021-09-16 11:26:19 -0400

    added register feature

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,53 @@
 var express = require('express');
+const bcrypt = require('bcrypt')
 var router = express.Router();
+const db = require('../models');
+const { Op } = require('sequelize')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* Register new user */
+router.post('/register', function (req, res, next) {
+  // take username, password, and email
+  if (!req.body.username || !req.body.email || !req.body.password) {
+    res.status(400).json({
+      error: 'Please include username, email and password'
+    })
+    return
+  }
+  // create a user
+  // check if username is taken
+  db.User.findOne({
+    where: {
+      [Op.or]: {
+        username: req.body.username,
+        email: req.body.email
+      }
+
+    }
+  })
+    .then((user) => {
+      if(user) {
+        res.status(400).json({
+          error: 'Username or email taken'
+        })
+        return
+      }
+      // hash password
+      bcrypt.hash(req.body.password, 10)
+        .then((hash) => {
+          // store in database
+          db.User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash
+          })
+            .then((user) => {
+              // respond with success/error message
+              res.status(201).json({
+                success: 'User created'
+              })
+            })
+        })
+    })
 });
 
 module.exports = router;
