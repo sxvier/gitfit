392ede16e0fc8edb84e439e8840d58b14f7ae320
SemaJ Solomon <sxviersolomon@gmail.com>
2021-09-16 15:04:17 -0400

    added login feature and setup sessions

--- routes/users.js
+++ routes/users.js
@@ -50,4 +50,47 @@ router.post('/register', function (req, res, next) {
     })
 });
 
+router.post('/login', async (req, res) => {
+  // check if username and password  exist
+  if (!req.body.username || !req.body.email || !req.body.password) {
+    res.status(400).json({
+      error: 'Please include username, email and password'
+    })
+    return
+  }
+
+  // find user by username and email
+  const user = await db.User.findOne({
+    where: {
+      [Op.or]: {
+        username: req.body.username,
+        email: req.body.email
+      }
+    }
+  })
+
+  if (!user) {
+    res.status(400).json({
+      error: 'Could not find user with that username or email'
+    })
+    return
+  }
+
+  // check password
+  const success = await bcrypt.compare(req.body.password, user.password)
+  if (!success) {
+    res.status(401).json({
+      error: 'Incorrect password'
+    })
+    return
+  }
+  // login
+  req.session.user = user
+
+  // respond with success/error
+  res.json({
+    success: ' Successfully logged in'
+  })
+})
+
 module.exports = router;
